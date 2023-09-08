69898de74b8e16c5b626aed2cc6c4bea203405d4
filippov nikita <filippovnr73@gmail.com>
2023-09-08 09:51:05 +0300

    добавил вычитание

--- funcs.py
+++ funcs.py
@@ -4,8 +4,11 @@ print("Приветсвую, пользователь")
 def summ(a, b):
     return a + b
 
+def minus(a, b):
+    return a - b
+
 a = input("Скажи мне первое число ---> ")
 b = input("Скажи мне первое число ---> ")
 
-print(summ(a, b))
+
 
